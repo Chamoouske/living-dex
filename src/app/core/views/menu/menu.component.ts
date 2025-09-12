3534a67eeeda78fa88e590f90fb550103afb54b6
Ajax Lima <ajaxlima94@gmail.com>
2025-09-12 16:18:46 -0300

    feat: complementando o menu

--- src/app/core/views/menu/menu.component.ts
+++ src/app/core/views/menu/menu.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
-
+  public readonly routes = [
+    { path: '/', label: 'Página Inicial' },
+    { path: '/box', label: 'Boxes' }
+  ];
 }
